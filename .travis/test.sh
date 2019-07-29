94b51aa7e839cdcbef40c9b6b6795f166dbb0b3f
Nancy Chauhan <nancy.ext@Admins-MacBook-Pro-8.local>
2019-07-29 08:40:44 +0530

    Added correct fusesoc library, i.e. or1k_morcchino

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,7 +6,7 @@ export HOME=/tmp
 fusesoc init -y
 fusesoc library add mor1kx-generic https://github.com/stffrdhrn/mor1kx-generic.git
 fusesoc library add intgen https://github.com/stffrdhrn/intgen.git
-fusesoc library add mor1kx /src
+fusesoc library add or1k_marocchino /src
 
 cd $HOME/src/tools
 
